67c9e0d885028f165925c806dba36d663a069894
Alexander Goncharov <a.goncharov@buzzguru.com>
2023-01-24 01:58:24 +0300

    Fix handling errors

--- src/businessLogic/user.ts
+++ src/businessLogic/user.ts
@@ -1,12 +1,17 @@
 import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
 import { User } from "../models";
-import { wrongEmailOrPassword } from "../utils/const";
+import { duplicateEmailError, wrongEmailOrPassword } from "../utils/const";
 import { LoginParams, UserParams } from "../utils/types";
 import { getUser, insertUser } from "../repositories/user";
 import { insertToken } from "../repositories/token";
 
 export const addUser = async (user: UserParams) => {
+  const checkUser: User | null = await getUser(user.email);
+  if (checkUser) {
+    throw new Error(duplicateEmailError);
+  }
+    
   const encryptedUser: UserParams = { ...user };
   encryptedUser.password = bcrypt.hashSync(user.password, 10);
 
